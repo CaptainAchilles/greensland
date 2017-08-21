ba93850677193c6d555d64aa88c894c1d8380bd7
CaptainAchilles <nicholas-achilles@hotmail.com>
2017-08-21 19:59:23 +1000

    Added more appropriate documentation as per the documentation standards guidlines document

--- server/src/controllers/parkController.js
+++ server/src/controllers/parkController.js
@@ -1,9 +1,17 @@
 import ParkService from "../services/parkService";
 
+/**
+ * Attaches all park items to the given express Response parameter body as a stringified json object
+ * @return {Promise<Response>} Promise which resolves to the express Response
+ */
 const GetAll = (req, res) => ParkService.GetAll()
     .then(allParks => res.status(200).json(allParks))
     .catch(err => res.status(500).json(err));
 
+/**
+ * Attaches the park with an ID matching the id paramter of the given express Request parameter to the given express Response parameter body as a stringified json object
+ * @return {Promise<Response>} Promise which resolves to the express Response
+ */
 const GetByID = (req, res) => {
     const id = req.params.id;
     if (id === undefined) {
--- server/src/services/parkService.js
+++ server/src/services/parkService.js
@@ -1,6 +1,16 @@
 import ParkRepository from "../repositories/parkRepository";
 
-const GetAll = () => ParkRepository.GetAll()
+/**
+ * Fetches all Park models available
+ * @return {Promise<Park[]>} Promise which resolves to an array of Park models
+ */
+const GetAll = () => ParkRepository.GetAll();
+
+/**
+ * Fetches a single Park model which matches the given id
+ * @param {string} id The ID of the park to find
+ * @return {Promise<Park | undefined>} Promise which resolves to the park which matches the given ID, or undefined if not match was found
+ */
 const GetByID = id => ParkRepository.GetByID(id);
 
 module.exports = {
